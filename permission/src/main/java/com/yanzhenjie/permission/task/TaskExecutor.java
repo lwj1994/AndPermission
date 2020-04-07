6c325f7aadd2f6df8d254f8ba7317bfdda41261b
lwjlol <alwjlola@gmail.com>
2020-04-07 15:26:57 +0800

    remove waitDialog

--- permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
+++ permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
@@ -15,10 +15,8 @@
  */
 package com.yanzhenjie.permission.task;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.os.AsyncTask;
-
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -29,25 +27,16 @@ public abstract class TaskExecutor<T> extends AsyncTask<Void, Void, T> {
 
     private static Executor sExecutor = Executors.newSingleThreadExecutor();
 
-    private Dialog mDialog;
 
     public TaskExecutor(Context context) {
-        this.mDialog = new WaitDialog(context);
-        this.mDialog.setCancelable(false);
     }
 
     @Override
     protected final void onPreExecute() {
-        if (!mDialog.isShowing()) {
-            mDialog.show();
-        }
     }
 
     @Override
     protected final void onPostExecute(T t) {
-        if (mDialog.isShowing()) {
-            mDialog.dismiss();
-        }
         onFinish(t);
     }
 
